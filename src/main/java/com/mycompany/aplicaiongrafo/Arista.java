d9235b208dd1cce27f8c1d07adade76a7624f94a
Paulo <Paulo@DESKTOP-DKR3EVC>
2025-10-31 19:53:22 -0500

    Inicio

---
+++ src/main/java/com/mycompany/aplicaiongrafo/Arista.java
@@ -0,0 +1,25 @@
+
+package com.mycompany.aplicaiongrafo;
+
+public class Arista {
+    private final String destino;
+    private final int peso;
+
+    public Arista(String destino, int peso) {
+        this.destino = destino;
+        this.peso = peso;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    @Override
+    public String toString() {
+        return destino + "(" + peso + ")";
+    }
+}
